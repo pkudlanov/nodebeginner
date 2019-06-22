3fd87b1e9b71faf9f647a42020e6d02bf9eb5bf1
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-22 04:03:05 -0700

    put the starting server functionality in a function, exported it and started it from index.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var http = require('http');
 
-function onRequest(request, response) {
-    console.log('Request recieved.');
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log('Request recieved.');
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello World');
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started.');
+    
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started.');
+exports.start = start;
