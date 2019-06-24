afa539d9fe98f2fdc1ec3e5cd08cbf2b8176ab97
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-24 00:10:24 -0700

    added /show request handler, and mapped it to the url /show, displays png file correctly after it was added to the /tmp/test.png location

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,7 @@
-var querystring = require('querystring');
+var querystring = require('querystring'),
+    fs = require('fs');
 
-function start(response) {
+function start(response, postData) {
     console.log('Request handler \'start\' was called.');
 
     var body = '<html>' +
@@ -29,5 +30,12 @@ function upload(response, postData) {
     response.end();
 }
 
+function show(response) {
+    console.log('Request handler \'show\' was called.');
+    response.writeHead(200, {'Content-Type': 'image/png'});
+    fs.createReadStream('/tmp/test.png').pipe(response);
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
